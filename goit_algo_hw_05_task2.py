709b16e070e1fd7c2acbdd7bdd01714bebe7ef0c
Yevhen-hub <chumak2008evgeniy@gmail.com>
2026-06-03 12:11:08 +0200

    Add files via upload

---
+++ goit_algo_hw_05_task2.py
@@ -0,0 +1,28 @@
+# Task 2
+import re
+from typing import Callable
+
+
+def generator_numbers(text: str):
+    pattern = r' \d+\.\d+ |\ \d+\ '
+
+    matches = re.findall(pattern, f" {text} ")
+
+    for match in matches:
+        yield float(match.strip())
+
+
+def sum_profit(text: str, func: Callable) -> float:
+    total = 0
+
+    for number in func(text):
+        total += number
+
+    return total
+
+
+text = "Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів."
+
+total_income = sum_profit(text, generator_numbers)
+
+print(f"Загальний дохід: {total_income}")
